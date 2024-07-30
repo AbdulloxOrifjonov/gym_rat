ad338f6708ec262a345a2d8d53012de76d70b731
AbdulloxOrifjonov <abdulloxstrike1@gmail.com>
2024-07-30 16:53:47 +0500

    new branch

--- src/Pages/common/Members.jsx
+++ src/Pages/common/Members.jsx
@@ -1,9 +1,36 @@
 /** @format */
 
 import React from "react";
+import { Label, TextInput } from "flowbite-react";
+import { Button } from "flowbite-react";
 
 function Members() {
-  return <div>Members</div>;
+  return (
+    <div className="flex max-w-md flex-col gap-4">
+      <div>
+        <div className="mb-2 block">
+          <Label htmlFor="username3" color="success" value="Your name" />
+        </div>
+        <div className="flex gap-4">
+          <TextInput
+            id="username"
+            placeholder="Search first or last name . . ."
+            required
+            color="success"
+            className="w-80"
+            // helperText={
+            //   <>
+            //     <span className="font-medium">Alright!</span> Search first or last name . . .
+            //   </>
+            // }
+          />
+          <Button className="w-52" type="submit">
+            Add Member
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Members;
