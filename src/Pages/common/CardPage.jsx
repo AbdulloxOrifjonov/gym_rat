855ab4cfd837532edc2a9f9f8d468d4a3a139c10
AbdulloxOrifjonov <abdulloxstrike1@gmail.com>
2024-08-08 17:43:57 +0500

    new

--- src/Pages/common/CardPage.jsx
+++ src/Pages/common/CardPage.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+/** @format */
 
-function CardPage() {
+import React from "react";
+import { useParams } from "react-router-dom";
+
+function CardPage({ products }) {
+  console.log(products);
+  const cardId = useParams();
+  const filteredCard = products?.filter((prod) => cardId.id === prod.id);
+
+  console.log(cardId);
+  console.log(filteredCard);
+
   return (
     <div>
-      
+      {filteredCard?.map((card) => (
+        <h2>{card.name}</h2>
+      ))}
     </div>
-  )
+  );
 }
 
-export default CardPage
+export default CardPage;
