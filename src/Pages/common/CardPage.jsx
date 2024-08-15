6cc47a542d0664c331db1b7d4eea3247e25f2d7f
AbdulloxOrifjonov <abdulloxstrike1@gmail.com>
2024-08-15 15:07:59 +0500

    cardpage

--- src/Pages/common/CardPage.jsx
+++ src/Pages/common/CardPage.jsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import React from "react";
 import { useParams } from "react-router-dom";
 
@@ -55,17 +57,17 @@ function CardPage() {
   console.log(products);
   console.log(cardId);
 
-  const filteredProduct = products?.filter(
-    (product) => productId === product.id
+  const filteredProduct = products?.filter((product) => cardId === product.id);
+  return (
+    <>
+      {filteredProduct?.map((product) => (
+        <div>
+          <h2>{product.name}</h2>
+          <h2>{product.cost}</h2>
+        </div>
+      ))}
+    </>
   );
-  return <>
-    {filteredProduct?.map((product) => (
-      <div>
-        <h2>{product.name}</h2>
-        <h2>{product.cost}</h2>
-      </div>
-    ))}
-  </>
 }
 
 export default CardPage;
