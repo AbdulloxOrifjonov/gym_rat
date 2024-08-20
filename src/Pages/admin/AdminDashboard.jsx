9bb071d343b7e3ec4982ab6159a11052d8f9989d
AbdulloxOrifjonov <abdulloxstrike1@gmail.com>
2024-08-20 17:41:51 +0500

    owner login qoldi

--- src/Pages/admin/AdminDashboard.jsx
+++ src/Pages/admin/AdminDashboard.jsx
@@ -1,9 +1,42 @@
 /** @format */
 
-import React from "react";
-import { Table } from "flowbite-react";
+import React, { useEffect, useState } from "react";
+import { Table, Pagination } from "flowbite-react";
+import axios from "axios";
 
 function AdminDashboard() {
+  const [owners, setOwners] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() => {
+    const fetchOwners = async () => {
+      try {
+        const response = await axios.get(
+          `https://gymrat.uz/api/v1/employer/pagination?page=${currentPage}&pageSize=10`,
+          {
+            headers: {
+              authorization: localStorage.getItem("token"),
+            },
+          },
+        );
+        console.log(response.data.data);
+        setOwners(response.data.data);
+        setTotalPages(response.data.employersCount); // API dan totalPages ni oling
+      } catch (error) {
+        console.error("Error fetching owners:", error);
+      }
+    };
+
+    fetchOwners();
+  }, [currentPage]);
+
+  const onPageChange = (page) => {
+    if (page >= 1 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
   return (
     <div>
       <div className="overflow-x-auto">
@@ -11,63 +44,37 @@ function AdminDashboard() {
           <Table.Head>
             <Table.HeadCell>Fullname</Table.HeadCell>
             <Table.HeadCell>Phone number</Table.HeadCell>
-            <Table.HeadCell>Gym name</Table.HeadCell>
             <Table.HeadCell>Delete vs Edit</Table.HeadCell>
           </Table.Head>
           <Table.Body className="divide-y">
-            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                {"Abdullox"}
-              </Table.Cell>
-              <Table.Cell>+998330119901</Table.Cell>
-              <Table.Cell>Anjumaniya</Table.Cell>
-              <Table.Cell>
-                <div className="flex items-center gap-2">
-                  <button className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                    Edit
-                  </button>
-                  <button className="font-medium text-red-600 hover:underline dark:text-red-500">
-                    Delete
-                  </button>
-                </div>
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                Ayub
-              </Table.Cell>
-              <Table.Cell>+998900000000</Table.Cell>
-              <Table.Cell>damolish</Table.Cell>
-              <Table.Cell>
-                <div className="flex items-center gap-2">
-                  <button className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                    Edit
-                  </button>
-                  <button className="font-medium text-red-600 hover:underline dark:text-red-500">
-                    Delete
-                  </button>
-                </div>
-              </Table.Cell>
-            </Table.Row>
-            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                Rustam
-              </Table.Cell>
-              <Table.Cell>+998335544878</Table.Cell>
-              <Table.Cell>Kok jiguli</Table.Cell>
-              <Table.Cell>
-                <div className="flex items-center gap-2">
-                  <button className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
-                    Edit
-                  </button>
-                  <button className="font-medium text-red-600 hover:underline dark:text-red-500">
-                    Delete
-                  </button>
-                </div>
-              </Table.Cell>
-            </Table.Row>
+            {owners?.map((owner) => (
+              <Table.Row key={owner.id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  {owner.fullname}
+                </Table.Cell>
+                <Table.Cell>{owner.phone}</Table.Cell>
+                <Table.Cell>
+                  <div className="flex items-center gap-2">
+                    <button className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                      Edit
+                    </button>
+                    <button className="font-medium text-red-600 hover:underline dark:text-red-500">
+                      Delete
+                    </button>
+                  </div>
+                </Table.Cell>
+              </Table.Row>
+            ))}
           </Table.Body>
         </Table>
+      </div>
+      <div className="flex overflow-x-auto sm:justify-center">
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages > 0 ? totalPages : 1} // totalPages 1 yoki undan katta bo'lishini ta'minlash
+          onPageChange={onPageChange}
+          className="pagination"
+        />
       </div>
     </div>
   );
